df01c16e8bf2eb63f11d4d2f3efd84e5d56b9195
Farid <farid.fv1@gmail.com>
2024-02-10 23:25:33 +0400

    Updated code to fix bug #123

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -1,38 +1,32 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import '../Components/LogIn.css'
-
+ 
 const SignUpPage = () => {
-  const [password, setPassword] = useState('')
-  const [name, setName] = useState('')
-  const [surname, setSurname] = useState('')
-  const [email, setEmail] = useState('')
-
-  async function signUp() {
-    let item = {
-      name, 
-      surname,
-      email,
-      password,
-    }
-    const userData = JSON.stringify(item)
-
-    localStorage.setItem('user', userData)
+  const [userData, setUserData] = useState({ name: "", surname: "", email: "", password: "" })
+ 
+  const handleChange = (type, data) => {
+    setUserData(prev => ({ ...prev, ...{ [type]: data } }))
   }
-  
-
+ 
+  function signUp(e) {
+    e.preventDefault()
+    const users = localStorage.getItem("users")
+    localStorage.setItem('users', users ? JSON.stringify([...JSON.parse(users), userData]) : JSON.stringify([userData]))
+  }
+ 
   return (
     <div className='container'>
       <h1 className='title'>Sign Up</h1>
       <form onSubmit={signUp} id='login-form'>
-        
-        <input type='text' placeholder='Name'  required onChange={(e)=>setName(e.target.value)} />
-        <input type='text' placeholder='Surname'  required onChange={(e)=>setSurname(e.target.value)} />
-        <input type='email' placeholder='Email' required onChange={(e)=>setEmail(e.target.value)} />
-        <input type='password' placeholder='Password' required onChange={(e)=>setPassword(e.target.value)} />
+ 
+        <input type='text' placeholder='Name' required onChange={(e) => handleChange("name", e.target.value)} />
+        <input type='text' placeholder='Surname' required onChange={(e) => handleChange("surname", e.target.value)} />
+        <input type='email' placeholder='Email' required onChange={(e) => handleChange("email", e.target.value)} />
+        <input type='password' placeholder='Password' required onChange={(e) => handleChange("password", e.target.value)} />
         <button className='btn'>Log in</button>
       </form>
     </div>
   )
 }
-
+ 
 export default SignUpPage
